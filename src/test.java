328fb85d66677839f55220a65c555e24fbd4e0f5
Fireye <codekai16@gmail.com>
2023-01-15 01:30:54 -0700

    new

--- src/test.java
+++ src/test.java
@@ -2,9 +2,10 @@ public class test {
 
     public static void main(String[] args) {
 
+        
         // TODO oooooo
         // assignees: Fireye04
-        // labels: impossible
+        // labels: impossible, dumb
         // description here
 
         System.out.println("ab");
