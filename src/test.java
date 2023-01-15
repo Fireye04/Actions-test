866784e0b16c1d0eb3983ff087c6687b3bbf731e
Fireye <codekai16@gmail.com>
2023-01-15 02:30:47 -0700

    asfSVDfsad

--- src/test.java
+++ src/test.java
@@ -4,7 +4,7 @@ public class test {
 
 
         System.out.println("ab");
-
+        //TODO wheee
     }
 
 }
--- src/testt/test_but_better.java
+++ src/testt/test_but_better.java
@@ -2,7 +2,6 @@ package testt;
 
 public class test_but_better {
     public static void main(String[] args) {
-        //TODO get good?
-        //thing?
+        //TODO wheee
     }
 }
